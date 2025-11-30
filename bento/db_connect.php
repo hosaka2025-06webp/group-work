bf24e1fa7db83497d8f8d7e8dee3945008175f25
hosaka2025-06webp <fireflylight.led@gmail.com>
2025-11-30 22:01:10 +0900

    Update db_connect.php

--- bento/db_connect.php
+++ bento/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 function getDb(): PDO
 {
-	$dsn = 'mysql:dbname=bento; host=127.0.0.1; charset=utf8';
+	$dsn = 'mysql53.conoha.ne.jp; charset=utf8';
 	$usr = 'root';
 	$passwd = '12345';
 	$db = new PDO($dsn, $usr, $passwd);
